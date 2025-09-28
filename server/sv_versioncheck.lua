b99d82271d82aa9ea94546f3b20bdf6fd2bb3375
2Whiffy <lukemaxwell1@hotmail.co.uk>
2025-09-28 03:56:11 +0100

    .

--- server/sv_versioncheck.lua
+++ server/sv_versioncheck.lua
@@ -23,7 +23,7 @@ local function checkResourceVersion(err, responseText, headers)
     
     if currentVersionFile.version >= remoteVersionFile.version then
         print("^5======================================^7")
-        print("^2[it-crafting] - The Script is up to date!")
+        print("^2[2WhiffyCrafting] - The Script is up to date!")
         print("^7Current Version: ^4" .. remoteVersionFile.version .. "^7.")
         print('^7Branch: ^4'..Config.Branch.."^7.")
         print("^5======================================^7")
@@ -31,7 +31,7 @@ local function checkResourceVersion(err, responseText, headers)
     end
 
     print("^5======================================^7")
-    print('^8[it-crafting] - New update available now!')
+    print('^8[2WhiffyCrafting] - New update available now!')
     print('^7Current Version: ^4'..currentVersionFile.version..'^7.')
     print('^7New Version: ^4'..remoteVersionFile.version..'^7.')
     print('^7Branch: ^4'..Config.Branch.."^7.")
@@ -40,13 +40,13 @@ local function checkResourceVersion(err, responseText, headers)
     print('^4Download it now on https://github.com/'..updatePath)
     print("^5======================================^7")
 
-    SendToWebhook(0, 'message', nil, {description = '### [it-crafting] - New update available now!\n **Current Version:** '..currentVersionFile.version..'\n **New Version:** '..remoteVersionFile.version..'\n **Branch:** '..Config.Branch..'\n **Notes:** '..remoteVersionFile.message..'\n **Download it now on:** [GitHub](https://github.com/'..updatePath..')'})
+    SendToWebhook(0, 'message', nil, {description = '### [2WhiffyCrafting] - New update available now!\n **Current Version:** '..currentVersionFile.version..'\n **New Version:** '..remoteVersionFile.version..'\n **Branch:** '..Config.Branch..'\n **Notes:** '..remoteVersionFile.message..'\n **Download it now on:** [GitHub](https://github.com/'..updatePath..')'})
 end
 
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() and Config.EnableVersionCheck then
         Wait(3000)
-        updatePath = "it-scripts/it-crafting"
+        updatePath = "2Whiffy/2WhiffyCrafting"
         local branch = Config.Branch
         PerformHttpRequest("https://raw.githubusercontent.com/"..updatePath.."/"..branch.."/version", checkResourceVersion, "GET")
     end
